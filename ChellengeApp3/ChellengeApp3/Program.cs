06fd372c79558accadc26edd736011354db77085
emgor <emgor@protonmail.com>
2023-06-11 23:02:28 +0200

    Zadanie domowe dzień 4

--- ChellengeApp3/ChellengeApp3/Program.cs
+++ ChellengeApp3/ChellengeApp3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool isWoman = true;
+var Age = 17;
+
+if (Age < 30 && isWoman == true)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (Age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (Age < 18 && isWoman == false)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
